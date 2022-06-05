ed7181f78f173fa7d5052fb4a7762a93adab89cd
Hoàng Đức <hoanghuuduc22@gmail.com>
2022-06-05 08:48:03 +0700

    uncomment code

--- GetImagesFromVieo.py
+++ GetImagesFromVieo.py
@@ -2,13 +2,13 @@ import cv2
 import time
 
 def main():
-    cap = cv2.VideoCapture('video/BanDuc01.mp4')
+    cap = cv2.VideoCapture('video/BanVanTinh01.mp4')
     # Resolution 640*480
     time.sleep(1)
     if cap is None or not cap.isOpened():
         print('Khong the mo file video')
         return
-    cv2.namedWindow('Image', cv2.WINDOW_AUTOSIZE);
+    cv2.namedWindow('Image', cv2.WINDOW_AUTOSIZE)
     n = 1
     dem = 200
     while True:
@@ -16,14 +16,14 @@ def main():
         print(success)
         ch = cv2.waitKey(30)
         if success:
-            imgROI = img[40:(40+480),:] # Tao ra anh 480x480
+            imgROI = img[40:(40+480),:]
 
             imgROI = cv2.resize(imgROI,(250,250))
             cv2.imshow('Image', imgROI)
         else:
             break
         if n%4 == 0:
-            filename = 'image/BanDuc/BanDuc_%04d.bmp'%(dem)
+            filename = 'image/BanVanTinh/BanVanTinh_%04d.bmp'%(dem)
             cv2.imwrite(filename,imgROI)
             dem = dem + 1
         n = n + 1
